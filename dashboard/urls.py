08c7de986b854e4741a50f49f22b43f098d72802
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-07-01 23:58:20 +0600

    feat: prepare data for ui visualization

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("get_students/", views.get_students, name="get_students"),
+    path("add_student/", views.add_student, name="add_student"),
+    path("get_distractions/", views.get_distractions, name="get_distractions"),
+    path("add_distraction/", views.add_distraction, name="add_distraction"),
+]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,56 @@
-from django.http import HttpResponse, JsonResponse
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Student, Distraction
 
 
 def index(request):
     return JsonResponse({"message": "Hello, world. You're at the dashboard index."})
+
+
+def get_students(request):
+    # return all students as jsonresponse
+    return JsonResponse({"students": list(Student.objects.values())})
+
+
+@csrf_exempt
+def add_student(request):
+    # get name parameter from request, check for post request
+    name = request.GET.get("name")
+    if request.method == "POST":
+        student = Student.objects.create(name=name)
+        data = {
+            "id": student.id,
+            "name": student.name,
+            "distractions": [],
+        }
+        return JsonResponse({"message": data})
+    else:
+        # wrong request method error
+        return JsonResponse({"message": "Wrong request method."})
+
+
+def get_distractions(request):
+    # return name parameter from request as jsonresponse
+    name = request.GET.get("name")
+    student = Student.objects.get(name=name)
+    return JsonResponse({"distractions": list(student.distractions.values())})
+
+
+@csrf_exempt
+def add_distraction(request):
+    # get student by name parameter from request, create distraction with student and current datetime, check for post request
+    name = request.GET.get("name")
+    student = Student.objects.get(name=name)
+    if request.method == "POST":
+        distraction = Distraction.objects.create(student=student, time=datetime.now())
+        data = {
+            "id": distraction.id,
+            "student": distraction.student.name,
+            "time": distraction.time,
+        }
+        return JsonResponse({"message": data})
+    else:
+        # wrong request method error
+        return JsonResponse({"message": "Wrong request method."})
